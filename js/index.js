48b2ec2327acbb39a54f978987dae051efddcf22
lisashare <lisa_zhu2012@foxmail.com>
2018-03-24 21:39:10 +0800

    '完善了ES6的封装函数'

--- js/index.js
+++ js/index.js
@@ -95,3 +95,96 @@ Slider.prototype = {
 	}
 }
 //ES6
+class Slide{
+	//原型方法
+	constructor(id){
+		//属性
+		this.ele = $(id);
+		this.ullis = $get($get(this.ele,"ul")[0],"li");
+		this.num = this.ullis.length;
+		this.ollis = this.createElement();
+		this.indexX = 0;
+		this.slid();
+		this.ltBtn = $("ltBtn");
+		this.rtBtn = $("rtBtn");
+		this.addEvent();
+		this.timer = null;
+		this.autoPlay();
+	}
+	createElement(){
+		let ol = $create("ol");
+		let arrLi = [];
+		for(let i = 0;i < this.num;i ++){
+			let li = $create("li");
+			ol.appendChild(li);
+			arrLi.push(li);
+		}
+		this.ele.appendChild(ol);
+		//左右按钮
+		var leftSpan = $create('span');
+		leftSpan.innerHTML = '&lt;';
+		leftSpan.id = 'ltBtn';
+		this.ele.appendChild(leftSpan);
+		
+		var rightSpan = $create('span');
+		rightSpan.innerHTML = '&gt;';
+		rightSpan.id = 'rtBtn';
+		this.ele.appendChild(rightSpan);
+		
+		//文字背景
+		var oDiv = $create('div');
+		oDiv.id = 'msg';
+		this.ele.appendChild(oDiv);
+		return arrLi;
+	}
+	slid(){
+		for(let i = 0;i < this.num;i++){
+			this.ullis[i].style.display = 'none';
+			this.ollis[i].style.backgroundColor = 'red';
+		}
+		this.ullis[this.indexX].style.display = 'block';
+		this.ollis[this.indexX].style.backgroundColor = 'blue';
+		this.div = $("msg");
+		this.div.innerHTML = this.ullis[this.indexX].firstElementChild.firstElementChild.alt;
+	}
+	addEvent(){
+		this.ltBtn.onclick = function(){
+			this.indexX --;
+			if(this.indexX == -1){
+				this.indexX = this.num - 1;
+			}
+			this.slid();
+		}.bind(this);
+		this.rtBtn.onclick = function(){
+			this.indexX ++;
+			if(this.indexX == this.num){
+				this.indexX = 0;
+			}
+			this.slid();
+		}.bind(this);
+		
+		for(let i = 0;i < this.num;i ++){
+			var that = this;
+			this.ollis[i].onmouseenter = function(){
+				that.indexX = i;
+				that.slid();
+			};
+		}
+	}
+	//自动轮播
+	autoPlay(){
+		this.timer = setInterval(function(){
+			this.indexX ++;
+			if(this.indexX == this.num){
+				this.indexX = 0;
+			}
+			this.slid();
+		}.bind(this),3000)
+		this.ele.onmouseenter = function(){
+			clearInterval(this.timer);
+		}.bind(this);
+		this.ele.onmouseleave = function(){
+			this.autoPlay();
+		}.bind(this);
+	}
+}
